142a9791e7f3f9b2b587fa37bb88d82bb882d072
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2020-07-06 22:11:22 -0400

    Update main.cpp
    Remembering previous max in this version. It improved a performance drastically.

--- main.cpp
+++ main.cpp
@@ -2,17 +2,24 @@ class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
         vector<int> ret(arr.size());
+        int lastMaxIndex = -1;
         
         for (int i = 0; i < arr.size(); ++i) {
             int max = -1;
             
-            if (i + 1 < arr.size()) {
-                max = arr[i + 1];
+            if (lastMaxIndex > i) {
+                max = arr[lastMaxIndex];
             }
+            else {
+                if (i + 1 < arr.size()) {
+                    max = arr[i + 1];
+                }
             
-            for (int j = i + 2; j < arr.size(); ++j) {
-                if (arr[j] > max) {
-                    max = arr[j];
+                for (int j = i + 2; j < arr.size(); ++j) {
+                    if (arr[j] > max) {
+                        max = arr[j];
+                        lastMaxIndex = j;
+                    }
                 }
             }
             ret[i] = max;
