661f0f39e760883f0d383f2134e2e42e7dda60d1
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2020-07-06 22:32:37 -0400

    Update main.java
    Iterating from left to right.

--- main.java
+++ main.java
@@ -1,28 +1,16 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-         int lastMaxIndex = -1;
+        int previousMax = -1;
         
-        for (int i = 0; i < arr.length; ++i) {
-            int max = -1;
+        for (int i = arr.length - 1; i >= 0; --i) {
+            int currentElement = arr[i];
             
-            if (lastMaxIndex > i) {
-                max = arr[lastMaxIndex];
-            }
-            else {
-                if (i + 1 < arr.length) {
-                    max = arr[i + 1];
-                }
+            arr[i] = previousMax;
             
-                for (int j = i + 2; j < arr.length; ++j) {
-                    if (arr[j] > max) {
-                        max = arr[j];
-                        lastMaxIndex = j;
-                    }
-                }
+            if (currentElement > previousMax) {
+                previousMax = currentElement;
             }
-            arr[i] = max;
         }
         return arr;
-       
     }
 }
